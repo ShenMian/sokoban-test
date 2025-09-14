3e25c967f185029411f370d032b45755347afc5b
ShenMian <sms_school@outlook.com>
2025-09-14 19:17:54 +0100

    feat(audio): add mute when not focused option and output device selection
    - Add MUTE_WHEN_NOT_FOCUSED and OUTPUT options to translation file
    - Implement mute when not focused functionality in audio settings
    - Add output device selection to audio settings
    - Update UI to include new audio settings options

--- scripts/settings/audio.gd
+++ scripts/settings/audio.gd
@@ -1,8 +1,9 @@
 extends CenterContainer
 
-@onready var master_volumn: HSlider = $VBox/GridContainer/MasterVolumn
-@onready var music_volumn: HSlider = $VBox/GridContainer/MusicVolumn
-@onready var sfx_volumn: HSlider = $VBox/GridContainer/SfxVolumn
+@onready var master_volumn: HSlider = $VBox/VolumnGrid/MasterVolumn
+@onready var music_volumn: HSlider = $VBox/VolumnGrid/MusicVolumn
+@onready var sfx_volumn: HSlider = $VBox/VolumnGrid/SfxVolumn
+@onready var mute_when_not_focused: CheckButton = $VBox/OutputGrid/MuteWhenNotFocused
 
 
 func _ready() -> void:
@@ -17,3 +18,28 @@ func _ready() -> void:
 	master_volumn.value_changed.connect(linear_to_db.bind(AudioServer.set_bus_volume_db.bind(master_bus_index)))
 	music_volumn.value_changed.connect(linear_to_db.bind(AudioServer.set_bus_volume_db.bind(music_bus_index)))
 	sfx_volumn.value_changed.connect(linear_to_db.bind(AudioServer.set_bus_volume_db.bind(sfx_bus_index)))
+
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_APPLICATION_FOCUS_OUT:
+			_on_window_minimized()
+
+		NOTIFICATION_APPLICATION_FOCUS_IN:
+			_on_window_restored()
+
+
+func _on_window_minimized() -> void:
+	if not mute_when_not_focused.button_pressed:
+		return
+	
+	var master_bus_index := AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_bus_index, true)
+
+
+func _on_window_restored() -> void:
+	if not mute_when_not_focused.button_pressed:
+		return
+	
+	var master_bus_index := AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_bus_index, false)
