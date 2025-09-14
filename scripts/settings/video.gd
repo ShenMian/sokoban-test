5d0a3f49d19b5462917f6b50f15f816d06e54133
ShenMian <sms_school@outlook.com>
2025-09-14 18:44:27 +0100

    feat(settings): add video settings and update translation
    - Add video settings tab with window mode and VSync options
    - Update translation file with new display-related keys
    - Create new video settings script

---
+++ scripts/settings/video.gd
@@ -0,0 +1,36 @@
+extends CenterContainer
+
+@onready var window_mode: OptionButton = $VBox/GridContainer/WindowMode
+@onready var vsync: CheckButton = $VBox/GridContainer/Vsync
+
+
+func _ready() -> void:
+	match DisplayServer.window_get_mode():
+		DisplayServer.WINDOW_MODE_WINDOWED:
+			window_mode.selected = 0
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			window_mode.selected = 1
+		DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			window_mode.selected = 2
+		_:
+			window_mode.selected = -1
+	
+	window_mode.item_selected.connect(_on_window_mode_selected)
+	vsync.toggled.connect(_on_vsync_toggled)
+
+
+func _on_window_mode_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+
+
+func _on_vsync_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
