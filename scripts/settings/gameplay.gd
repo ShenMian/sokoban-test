bf72b66aa8f7aee61e3f8b62b48bf7fcba89bb77
ShenMian <sms_school@outlook.com>
2025-09-16 12:28:29 +0100

    feat(settings): implement dynamic language selection
    - Remove hardcoded language options
    - Populate language list dynamically from TranslationServer
    - Update UI to display language names in their respective languages
    - Improve code maintainability and scalability for future language additions

--- scripts/settings/gameplay.gd
+++ scripts/settings/gameplay.gd
@@ -2,11 +2,22 @@ extends CenterContainer
 
 @onready var language: OptionButton = $VBox/LanguagePanel/Margin/HBox/OptionButton
 
+const LOCALE_TO_NAME = {
+	"en": "English",
+	"zh": "中文"
+}
+
 
 func _ready() -> void:
+	var locales = TranslationServer.get_loaded_locales()
+	for i in range(locales.size()):
+		var locale = locales[i]
+		language.add_item(LOCALE_TO_NAME[locale])
+		language.set_item_metadata(i, locale)
+
 	language.item_selected.connect(_on_language_item_selected)
 
 
 func _on_language_item_selected(index: int):
-	var selected = language.get_item_text(index)
+	var selected := str(language.get_item_metadata(index))
 	TranslationServer.set_locale(selected)
