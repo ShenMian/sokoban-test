23af29c2b012a3e7861a6ee36311f5ec07a7e1de
ShenMian <sms_school@outlook.com>
2025-09-16 21:56:13 +0100

    feat(player): add outline effect on mouse hover
    - Create outline shader and material for player
    - Update player scene to use new outline material
    - Modify player script to handle mouse enter/exit events
    - Adjust box script to extend Node3D instead of MeshInstance3D
    - Update gameplay scene to set mouse filter on root node

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,20 @@
-extends MeshInstance3D
+extends Node3D
+
+@onready var mesh_instance: MeshInstance3D = $Mesh
+@onready var area: Area3D = $Mesh/Area
+
+@export var outline_thickness: float = 0.1
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
+	area.mouse_entered.connect(_on_area_mouse_entered)
+	area.mouse_exited.connect(_on_area_mouse_exited)
 	pass
+
+
+func _on_area_mouse_entered():
+	mesh_instance.mesh.material.next_pass["shader_parameter/thickness"] = outline_thickness
+
+
+func _on_area_mouse_exited():
+	mesh_instance.mesh.material.next_pass["shader_parameter/thickness"] = 0.0
