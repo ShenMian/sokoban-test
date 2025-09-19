81e711e1a00cf54e787e0b80811a7e5dd5f5d4a2
ShenMian <sms_school@outlook.com>
2025-09-19 18:12:00 +0100

    refactor(level_map): update level map string

--- scripts/level_map.gd
+++ scripts/level_map.gd
@@ -5,6 +5,6 @@ extends LevelMap
 
 func _ready() -> void:
 	# self.load_from_string("DuLLrUUdrR");
-	self.load_from_string("rRRddrruLrULLruullllDDDuuurrrrddlLLrrrddlUruLLrruulllldDldRuuurrrrddllLrrruulllldDrrrrrruLdllllluurrrrDrdLLruullllddrDulldRuuurrrrddlLLrrruulllldD");
+	self.load_from_string("rRRddrruULuullllDDldRuuurrrrddLLLrrruulllldDDurDurrrrruLdllllluurrrrDrdLLLLrrddrUruululllldDldRurrrdrruLLLLrrruulllldD");
 	camera.global_position.x = self.dimensions().x / 2.0
 	camera.global_position.z = self.dimensions().y / 2.0
