03af9771c742cc242cf0db70ee23555df5824b9d
ShenMian <sms_school@outlook.com>
2026-01-18 15:38:24 +0000

    fix(level_selector): correct item numbering

--- scripts/level_selector.gd
+++ scripts/level_selector.gd
@@ -12,7 +12,7 @@ func _ready():
 	item_list.resized.connect(_on_item_list_resized)
 	for i in range(200):
 		var placeholder = PlaceholderTexture2D.new()
-		item_list.add_item(str(i), placeholder, true)
+		item_list.add_item(str(i + 1), placeholder, true)
 
 
 func _process(_delta: float):
@@ -28,16 +28,17 @@ func _process(_delta: float):
 	end_item_index = end
 
 	for i in range(start, end + 1):
-		item_list.set_item_icon(i, load("res://assets/icon.svg"))
+		var icon = load("res://assets/icon.svg")
+		item_list.set_item_icon(i, icon)
 
 
 func _on_item_list_resized():
 	var content_width = get_content_width(item_list)
 	var h_separation = item_list.get_theme_constant("h_separation")
-	
+
 	var items_per_row = int(content_width / item_min_width)
 	var item_width = content_width / items_per_row - h_separation
-	
+
 	item_list.max_columns = items_per_row
 	item_list.set_fixed_column_width(item_width)
 	item_list.set_fixed_icon_size(Vector2(item_width, item_width))
@@ -45,14 +46,14 @@ func _on_item_list_resized():
 
 func get_content_width(item_list: ItemList) -> float:
 	var width = item_list.size.x
-	
+
 	# Subtract left and right margins
 	var style_box = item_list.get_theme_stylebox("panel")
 	if style_box:
 		width -= style_box.get_margin(SIDE_LEFT) + style_box.get_margin(SIDE_RIGHT)
-	
+
 	# Subtract scrollbar width
 	var v_scroll_bar = item_list.get_v_scroll_bar()
 	width -= v_scroll_bar.size.x
-	
+
 	return width
