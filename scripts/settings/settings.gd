ed5c5dc51b9bcd7355da47d10d32153f536e0bc3
ShenMian <sms_school@outlook.com>
2026-03-04 13:17:29 +0000

    feat(settings): add config file validation checks

--- scripts/settings/settings.gd
+++ scripts/settings/settings.gd
@@ -4,16 +4,10 @@ signal setting_changed(section: String, key: String, value: Variant)
 
 const INT_MAX := Vector3i.MAX.x
 
-var config := ConfigFile.new()
-const CONFIG_PATH = "user://settings.ini"
-
-var solutions := ConfigFile.new()
-const SOLUTIONS_PATH = "user://solutions.ini"
-
 const DEFAULT_CONFIG = {
 	"gameplay": {
 		"language": "en",
-		"animation_speed": 1,
+		"animation_speed": 0,
 		"checkerboard": true,
 		"deadlock_hint": true,
 		"pushable_hint": true,
@@ -46,23 +40,35 @@ const DEFAULT_BINDINGS_PATH = "user://default_bindings.tres"
 const BINDINGS_PATH := "user://bindings.tres"
 const LEVEL_PATH := "res://assets/levels/"
 
+const CONFIG_PATH = "user://settings.ini"
+var _config := ConfigFile.new()
+
+const SOLUTIONS_PATH = "user://solutions.ini"
+var _solutions := ConfigFile.new()
+
 var current_collection
 var current_level_index
 
 
 func _ready():
-	var error := config.load(CONFIG_PATH)
-	if error:
-		printerr("failed to load config file: ", error_string(error))
+	print("Config file path: ", ProjectSettings.globalize_path(CONFIG_PATH))
 
-		# Creates default settings
+	var error := _config.load(CONFIG_PATH)
+	if error || !_is_config_valid(_config):
+		if error:
+			printerr("failed to load config file: ", error_string(error))
+		elif !_is_config_valid(_config):
+			printerr("config file structure is invalid or outdated", !_is_config_valid(_config))
+
+		# Resets to default settings
+		print("Restore default settings")
 		reset_gameplay_settings()
 		reset_video_settings()
 		reset_audio_settings()
 	
-	var solutions_error := solutions.load(SOLUTIONS_PATH)
+	var solutions_error := _solutions.load(SOLUTIONS_PATH)
 	if solutions_error:
-		printerr("failed to load solutions file: ", error_string(solutions_error))
+		printerr("failed to load _solutions file: ", error_string(solutions_error))
 
 	save_input_bindings(DEFAULT_BINDINGS_PATH)
 	load_input_bindings()
@@ -70,45 +76,45 @@ func _ready():
 
 func set_and_save_value(section: String, key: String, value: Variant):
 	set_value(section, key, value)
-	config.save(CONFIG_PATH)
+	_config.save(CONFIG_PATH)
 
 
 func set_value(section: String, key: String, value: Variant):
-	config.set_value(section, key, value)
+	_config.set_value(section, key, value)
 	setting_changed.emit(section, key, value)
 
 
 func get_value(section: String, key: String):
-	return config.get_value(section, key)
+	return _config.get_value(section, key)
 
 
 func reset_gameplay_settings():
-	config.erase_section("gameplay")
+	_config.erase_section("gameplay")
 	for key in DEFAULT_CONFIG.gameplay:
 		var value = DEFAULT_CONFIG.gameplay[key]
 		set_value("gameplay", key, value)
 
 	var locale = OS.get_locale()
 	if locale in TranslationServer.get_loaded_locales():
-		config.set_value("gameplay", "language", locale)
+		_config.set_value("gameplay", "language", locale)
 
-	config.save(CONFIG_PATH)
+	_config.save(CONFIG_PATH)
 
 
 func reset_video_settings():
-	config.erase_section("video")
+	_config.erase_section("video")
 	for key in DEFAULT_CONFIG.video:
 		var value = DEFAULT_CONFIG.video[key]
 		set_value("video", key, value)
-	config.save(CONFIG_PATH)
+	_config.save(CONFIG_PATH)
 
 
 func reset_audio_settings():
-	config.erase_section("audio")
+	_config.erase_section("audio")
 	for key in DEFAULT_CONFIG.audio:
 		var value = DEFAULT_CONFIG.audio[key]
 		set_value("audio", key, value)
-	config.save(CONFIG_PATH)
+	_config.save(CONFIG_PATH)
 
 
 func reset_input_settings():
@@ -161,8 +167,8 @@ func set_level_solution(collection: String, level: int, actions: String):
 	if solution["moves_optimal"].is_empty() or new_moves < best_moves:
 		new_solution["moves_optimal"] = actions
 
-	solutions.set_value(collection, str(level), new_solution)
-	solutions.save(SOLUTIONS_PATH)
+	_solutions.set_value(collection, str(level), new_solution)
+	_solutions.save(SOLUTIONS_PATH)
 
 
 const DEFAULT_SOLUTION := {
@@ -172,5 +178,24 @@ const DEFAULT_SOLUTION := {
 
 
 func get_level_solution(collection: String, level: int) -> Dictionary:
-	var solution: Dictionary = solutions.get_value(collection, str(level), DEFAULT_SOLUTION)
+	var solution: Dictionary = _solutions.get_value(collection, str(level), DEFAULT_SOLUTION)
 	return solution
+
+
+func _is_config_valid(config: ConfigFile) -> bool:
+	# Checks sections
+	if Array(config.get_sections()) != DEFAULT_CONFIG.keys():
+		return false
+
+	for section in DEFAULT_CONFIG:
+		# Checks keys
+		if Array(config.get_section_keys(section)) != DEFAULT_CONFIG[section].keys():
+			return false
+
+		# Checks value types
+		for key in DEFAULT_CONFIG[section]:
+			var current_value = config.get_value(section, key)
+			var default_value = DEFAULT_CONFIG[section][key]
+			if typeof(current_value) != typeof(default_value):
+				return false
+	return true
